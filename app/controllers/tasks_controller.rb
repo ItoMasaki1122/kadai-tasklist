d10e5e97cca90fe57b43365f227a16baa6a93094
masaki.itou <msk.nfl.teabow12@gmail.com>
2020-12-27 12:46:55 +0000

    add_login_security

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   
   def index
-    @task = current_user.tasks.build
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
@@ -58,4 +58,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:status, :content)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to login_url
+    end
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   
-  def index
-   # 今回は不要？
-  end
-
-  def show
-    @user = User.find(params[:id])
-    
-  end
-
   def new
     @user = User.new
   end
